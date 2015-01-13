2e934005ff867a47ef5e2b926c16c891aeb1032c
fabuloso <dalfovo.alessandro@gmail.com>
2015-01-13 22:00:18 +0100

    Initial Commit

---
+++ src/main/java/it/ale/AbsenteeService.java
@@ -0,0 +1,13 @@
+package it.ale;
+
+/**
+ * Hello world!
+ *
+ */
+public class AbsenteeService
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/it/ale/AbsenteeServiceTest.java
@@ -0,0 +1,13 @@
+package it.ale;
+
+import org.junit.Test;
+
+public class AbsenteeServiceTest
+{
+
+    @Test public void
+    test(){
+
+    }
+
+}
